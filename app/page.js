406a08c9a67826da922513fd8c5a98b58866ee0e
midCenturyDude7 <mgriffes@gmail.com>
2024-09-15 10:56:45 -0700

    Updated font and bg-color config

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20"></div>
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[sofia-pro,_sans-serif] font-bold not-italic text-2xl text-slate-300">
+      Hello World!
+    </div>
   );
 }
